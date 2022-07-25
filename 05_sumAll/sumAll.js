6723c2b7c717ca7c8005f2e240b6c28967473066
DXPGH <dxpbusiness@gmail.com>
2022-07-25 18:50:34 -0500

    Completed 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (start, iterations) {
+  let sum = 0;
 
+  //Checks if a value is negative or not
+  if (start <= -1 || iterations <= -1) {
+    return "ERROR";
+  }
+
+  //Checks if the values inputted to the function are a typeof number or not
+  if (isNaN(iterations) || typeof iterations !== "number") {
+    return "ERROR";
+  }
+
+  //Checks which value is the bigger number
+  if (start < iterations) {
+    for (let i = 0; i < iterations; i++) {
+      sum += start + i;
+    }
+    return sum;
+  } else {
+    for (let i = 0; i < start; i++) {
+      sum += iterations + i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
