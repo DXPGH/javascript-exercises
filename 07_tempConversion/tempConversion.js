68b77816c09ad4015fe0101f2422516d022f169f
DXPGH <dxpbusiness@gmail.com>
2022-07-25 19:08:30 -0500

    Completed 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fTemp) {
+  cTemp = ((fTemp - 32) * 5) / 9;
+  return Math.round(cTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (cTemp) {
+  fTemp = (cTemp * 9) / 5 + 32;
+  return Math.round(fTemp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
