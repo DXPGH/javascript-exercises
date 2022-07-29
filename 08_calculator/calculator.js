c7763286eaa6a8f0cab5e72d037c7a382c4ab193
DXPGH <dxpbusiness@gmail.com>
2022-07-29 10:36:23 -0500

    Completed 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = (array) => {
+  if (!array.length) {
+    return 0;
+  } else {
+    return array.reduce((a, b) => a + b);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
