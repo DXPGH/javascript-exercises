2e86b5a6364aff597732ba40c3771f4f6174cc91
DXPGH <dxpbusiness@gmail.com>
2022-07-29 10:50:21 -0500

    Completed 09-Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (args) {
+  //Lowercase the string and remove the unwanted characters from it
+  let re = /[\W_]/g;
+  let lowRegStr = args.toLowerCase().replace(re, "");
 
+  //Split the string into an array of single characters, Reverse the array, Rejoin the single characters into one
+  let reverseStr = lowRegStr.split("").reverse().join("");
+
+  //Check if the reverseStr is === to the lowRegStr and return the Boolean value
+  return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
